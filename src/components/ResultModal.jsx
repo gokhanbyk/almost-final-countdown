0bd1708f60ea8cda7718a0ff5f8d061a4c6d65d2
Gökhan Bıyıkoğlu <gokhan_byk@icloud.com>
2024-03-23 15:13:10 +0300

    using forwardRef for binding components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { forwardRef } from 'react';
 
-function ResultModal({ result, targetTime }) {
+const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
   return (
-    <dialog className='result-modal'>
+    <dialog ref={ref} className='result-modal'>
       <h2>You {result}</h2>
       <p>The target time was <strong>{targetTime} seconds.</strong></p>
       <p>You stopped the timer with <strong>X seconds left.</strong></p>
@@ -11,6 +12,6 @@ function ResultModal({ result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
 
-export default ResultModal;
+export default ResultModal; 
